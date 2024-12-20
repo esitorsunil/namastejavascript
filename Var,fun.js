c52c1c113ecae63ee7387ba864d1fe2171ab19e6
esitorsunil <sunilrio23@gmail.com>
2024-12-20 20:15:05 +0530

    Hoisting in JS (var& func)

---
+++ Var,fun.js
@@ -0,0 +1,13 @@
+getName();
+console.log(x);
+console.log(getName);
+console.log(getName2);
+
+var x = 7;
+function getName() {
+    console.log("Namaste JS")
+};
+
+var getName2 = function () {
+    console.log("Namaste")
+};
