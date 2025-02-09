bbee171808e01726f5e256cd85fd9a1101da8bfe
esitorsunil <sunilrio23@gmail.com>
2025-02-09 23:22:32 +0530

    relation of scope chain and closure

--- jsinterviewclosure.js
+++ jsinterviewclosure.js
@@ -1,10 +1,15 @@
 //closure eg
-
-function outer() {
-    var a = 10;
+function outest(){
+    var c = 20;
+function outer(b) {
+    
     function inner() {
-        console.log(a);
+        console.log(a, b, c);
     }
+    let a = 10;
     return inner;
 }
-outer()(); // 10
+    return outer;
+}
+var close = outest()("hello world"); // 10
+close();
