078d5eb51cdf36245bec9d3e376a872de72dca88
Sunil <135955149+esitorsunil@users.noreply.github.com>
2025-04-24 05:54:07 -0400

    Add files via upload

---
+++ Hoisted in JS/app.js
@@ -0,0 +1,17 @@
+
+getName()
+getNames() // Type error: getnames is not a func
+console.log(x)
+console.log(getName)
+console.log(getNames)
+
+
+var x =7
+function getName() {
+    var a = 10;
+}
+
+
+//var getName = () =>{console.log(10)} it will not work (same as in normal function)
+
+var getNames = () =>{console.log(10)} 
